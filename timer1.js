429c3ca1a37b856b1709707916cb0dae200ae6b7
Gokul Krishna <gokursx.123@gmail.com>
2024-05-11 15:29:20 -0700

    Added timer function

---
+++ timer1.js
@@ -0,0 +1,9 @@
+
+const inputArgs = process.argv.slice(2).filter(time => !isNaN(time) && time >= 0);
+
+inputArgs.forEach((time) => {
+  setTimeout(() => {
+    // This stdout method makes a beep sound
+    process.stdout.write('\x07'); // This stdout method makes a beep sound
+  }, time * 1000);
+});
